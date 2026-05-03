fdb1580e72bfe444be1a7a2a9b3bb334f118dc4c
maybe-adamant <maybe.adamant@gmail.com>
2026-05-03 11:56:01 -0400

    Update plugin guid usage

--- src/main.lua
+++ src/main.lua
@@ -17,6 +17,7 @@ local config = chalk.auto("config.lua")
 
 local PACK_ID = "speedrun"
 local MODULE_ID = "RunModsNPCs"
+local PLUGIN_GUID = _PLUGIN.guid
 
 ---@class RunModsNPCsInternal
 ---@field store ManagedStore|nil
@@ -73,6 +74,7 @@ local function init()
     internal.store = store
 
     lib.createModuleHost({
+        pluginGuid = PLUGIN_GUID,
         definition = definition,
         store = store,
         session = session,
@@ -81,7 +83,7 @@ local function init()
         drawTab = internal.DrawTab,
         drawQuickContent = internal.DrawQuickContent,
     })
-    internal.standaloneUi = lib.standaloneHost()
+    internal.standaloneUi = lib.standaloneHost(PLUGIN_GUID)
 end
 
 local loader = reload.auto_single()
